a31267db1f0c92c4179c83068bcb672eecd86580
Gerd Zellweger <mail@gerdzellweger.com>
2010-05-06 17:30:56 +0200

    some comments

--- mclab/buffer.c
+++ mclab/buffer.c
@@ -1,5 +1,15 @@
 /* Producer & Consumer
  * ====================
+ * Our producer/consumer code implements the following ideas:
+ *  - Make sure that concurrent threads access variables in different cache lines
+ *    as far as possible
+ *  - Use busy waiting instead of locking
+ *    This implies to have to variables head/tail for writes/reads in the buffer.
+ *  - Shared variables between producer and consumer are updated not every time
+ *    to minimize synchronization overhead
+ *  - Reduce the frequency of access to shared variables by introducing local
+ *    control variables (local_head & local_tail)
+ *
  * The implementation is a lock free shared ring buffer implementation
  * based on the paper "A Lock-Free, Cache-Efficient Shared Ring Buffer for
  * Multi-Core Architectures" by Patrick P. C. Lee, Tian Bu, Girish Chandranmenon.
@@ -37,65 +47,74 @@ typedef unsigned int uint;
 
 
 // Global Definitions & Variables
-#define CACHE_LINE 64
-#define BUFFER_SIZE 1024
-#define NEXT(current) ((current)+1) % (BUFFER_SIZE);
+#define CACHE_LINE 64 		// cache line is 64 bytes (this can vary from cpu to cpu)
+#define BUFFER_SIZE 1024 	// buffer size (we tried multiples of 2 and took the one which gave us the best results)
+#define BATCH_SIZE 128		// batch size, this means that buffer->head/tail are only updated every 128 inserts/updates
+							// (we tried multiples of 2 and took the one which gave us the best results)
+#define NEXT(current) ((current)+1) % (BUFFER_SIZE); // returns the next element in a circular buffer of size BUFFER_SIZE
 
 
-/*consumer’s local control variables*/
-struct consumer {
-	int localWrite;
-	int nextRead;
-	int rBatch;
-	char padding[CACHE_LINE-3*sizeof(int)];
+/**
+ * Variables used by the consumer.
+ **/
+static struct consumer {
+	int local_head;		// is always <= buffer->head
+	int next_tail;		// used to store actual tail until we update buffer->tail
+	int batch_counter;	// determines when to update buffer->tail
+	char padding[CACHE_LINE-3*sizeof(int)]; // make sure this struct is placed in its own cache line
 } c;
 
 
-/*producer’s local control variables*/
-struct producer {
-	int localRead;
-	int nextWrite;
-	int wBatch;
-	char padding[CACHE_LINE-3*sizeof(int)];
+/**
+ * Variables used by the producer.
+ **/
+static struct producer {
+	int local_tail;		// is always <= buffer->tail
+	int next_head;		// used to store actual head until we update buffer->head
+	int batch_counter;	// determines when to update buffer->head
+	char padding[CACHE_LINE-3*sizeof(int)]; // make sure this struct is placed in its own cache line
 } p;
 
-/*constants*/
-static int batchSize = 128;
-static char cache_pad3[CACHE_LINE - 1 * sizeof(int)];
 
+/**
+ * Buffer structure contains the data and the shared variables between
+ * the producer and consumer.
+ * Please note that head and tail do not actually have to correspond to the real
+ * head and tail values since we update them only after BATCH_SIZE read/writes
+ * to the buffer.
+ * Head and tail are marked as volatile since they're shared between
+ * the producer and consumer and it is vital that they're written
+ * back to memory every time they change.
+ */
 struct buffer_structure {
 	event_t storage[BUFFER_SIZE];
 
-	char cache_pad0[CACHE_LINE];
-	volatile int read;
-	volatile int write;
-	char cache_pad1[CACHE_LINE - 2 * sizeof(int)];
+	char padding0[CACHE_LINE];
+	volatile int tail;
+	volatile int head;
+	char padding1[CACHE_LINE - 2 * sizeof(int)];
 };
-//int producer_waited = 0;
-//int consumer_waited = 0;
+
 
 /** Allocates & initializes a buffer and returns it to the client.
  * @return buffer
  */
 buffer_ptr allocate_buffer() {
 
-
-	// dummy writes to make sure the compiler is not trying to be smart
 	buffer_ptr buffer = malloc( sizeof(buffer_t) );
 
 	if(buffer) {
-		buffer->read = 0;
-		buffer->write = 0;
-	}
-	c.localWrite = 0;
-	c.nextRead = 0;
-	c.rBatch = 0;
-	p.localRead = 0;
-	p.nextWrite = 0;
-	p.wBatch = 0;
+		buffer->tail = 0;
+		buffer->head = 0;
 
-	//producer_waited = 0;
-	//consumer_waited = 0;
+		// initialize variables for consumer/producer
+		c.local_head = 0;
+		c.next_tail = 0;
+		c.batch_counter = 0;
+		p.local_tail = 0;
+		p.next_head = 0;
+		p.batch_counter = 0;
+	}
 
     return buffer;
 }
@@ -105,53 +124,54 @@ void free_buffer(buffer_ptr buffer) {
     free(buffer);
 }
 
-void produce_event(buffer_ptr buffer, event_t element) {
-	//DEBUG_PRINT("produce\n");
 
-	int afterNextWrite = NEXT(p.nextWrite);
-	if (afterNextWrite == p.localRead) {
-		while (afterNextWrite == buffer->read) {
+void produce_event(buffer_ptr buffer, event_t element) {
+
+	int after_next_write = NEXT(p.next_head);
+	if (after_next_write == p.local_tail) {
+		// wait until consumer has read enough and we can write again
+		while (after_next_write == buffer->tail) {
 			/* busy waiting */
-			//producer_waited++;
 		}
-		p.localRead = buffer->read;
+		p.local_tail = buffer->tail;
 	}
 
-	buffer->storage[p.nextWrite] = element;
+	buffer->storage[p.next_head] = element;
+	p.next_head = after_next_write;
 
-	p.nextWrite = afterNextWrite;
-	p.wBatch++;
-
-	if (p.wBatch >= batchSize) {
-		buffer->write = p.nextWrite;
-		p.wBatch = 0;
+	// updates buffer->head every time batch counter reaches BATCH_SIZE
+	p.batch_counter++;
+	if (p.batch_counter >= BATCH_SIZE) {
+		buffer->head = p.next_head;
+		p.batch_counter = 0;
 	}
 
 }
+
 
 void produced_last_event(buffer_ptr buffer) {
-	buffer->write = p.nextWrite;
-	//DEBUG_PRINT("\nproducer waited: %d\nconsumer waited: %d\n", producer_waited, consumer_waited);
+	buffer->head = p.next_head;
 }
 
-event_t consume_event(buffer_ptr buffer) {
-	//DEBUG_PRINT("consume\n");
 
-	if(c.nextRead == c.localWrite) {
-		while(c.nextRead == buffer->write) {
-			/*busy waiting*/
-			//consumer_waited++;
+event_t consume_event(buffer_ptr buffer) {
+
+	if(c.next_tail == c.local_head) {
+		// wait until producer has produced something and we can read again
+		while(c.next_tail == buffer->head) {
+			/* busy waiting */
 		}
-		c.localWrite = buffer->write;
+		c.local_head = buffer->head;
 	}
 
-	event_t element = buffer->storage[c.nextRead];
-	c.nextRead = NEXT(c.nextRead);
+	event_t element = buffer->storage[c.next_tail];
+	c.next_tail = NEXT(c.next_tail);
 
-	c.rBatch++;
-	if(c.rBatch >= batchSize) {
-		buffer->read = c.nextRead;
-		c.rBatch = 0;
+	// updates buffer->tail every time batch counter reaches BATCH_SIZE
+	c.batch_counter++;
+	if(c.batch_counter >= BATCH_SIZE) {
+		buffer->tail = c.next_tail;
+		c.batch_counter = 0;
 	}
 
 	return element;
